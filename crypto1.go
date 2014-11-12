c4ca31634c9db538fce1ae376beebf3561bfb66d
Dan Shaffer <danielashaffer@gmail.com>
2014-11-11 21:54:19 -0800

    initial commit for set 1, parts 1-6

---
+++ crypto1.go
@@ -0,0 +1,159 @@
+// crypto1.go
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"io/ioutil"
+	"strings"
+)
+
+func HexToBase64(input string) string {
+	byte_arr, _ := hex.DecodeString(input)
+	return base64.StdEncoding.EncodeToString(byte_arr)
+}
+
+func FixedXOR(input1, input2 string) string {
+	bytes1, _ := hex.DecodeString(input1)
+	bytes2, _ := hex.DecodeString(input2)
+	out := make([]byte, len(bytes1))
+	for i := 0; i < len(bytes1); i++ {
+		out[i] = bytes1[i] ^ bytes2[i]
+	}
+	return hex.EncodeToString(out)
+}
+
+func GuessKey(input string) (key_b uint8, score_b int, str_b string) {
+	input_bytes, _ := hex.DecodeString(input)
+	return GuessKeyBytes(input_bytes)
+}
+
+func GuessKeyBytes(input_bytes []byte) (key_b uint8, score_b int, str_b string) {
+	best := ""
+	best_score := -1
+	best_key := uint8(0)
+	for key := uint8(0); key < uint8(255); key++ {
+		attempt := make([]byte, len(input_bytes))
+		for i := 0; i < len(input_bytes); i++ {
+			attempt[i] = input_bytes[i] ^ key
+		}
+		attempt_str := string(attempt)
+		freq := make(map[string]int)
+		for _, c := range strings.ToLower(attempt_str) {
+			freq[string(c)] += 1
+		}
+		score := 0
+		score += freq["e"]
+		score += freq["t"]
+		score += freq["a"]
+		score += freq["o"]
+		score += freq["i"]
+		score += freq["n"]
+		score += freq[" "]
+		if score > best_score {
+			best_score = score
+			best = attempt_str
+			best_key = key
+		}
+	}
+	return best_key, best_score, best
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func GuessWhich(filename string) (line int, result string) {
+	dat, err := ioutil.ReadFile(filename)
+	check(err)
+	lines := strings.Split(string(dat), "\n")
+	best_line_num := -1
+	best_score := -1
+	best_result := ""
+	for line_num, content := range lines {
+		_, score, decr := GuessKey(content)
+		if score > best_score {
+			best_line_num = line_num
+			best_score = score
+			best_result = decr
+		}
+	}
+	return best_line_num, best_result
+}
+
+func RepeatingKeyXOR(data string, key string) string {
+	data_hex := []byte(data)
+	key_hex := []byte(key)
+	key_len := len(key_hex)
+	result := []byte{}
+	for i, b := range data_hex {
+		relevant_key := key_hex[i%key_len]
+		result = append(result, b^relevant_key)
+	}
+	return hex.EncodeToString(result)
+}
+
+func GetBit(b byte, which uint) bool {
+	return ((b>>which)&byte(1) == byte(1))
+}
+
+func Hamming(s1 string, s2 string) int {
+	b1 := []byte(s1)
+	b2 := []byte(s2)
+	return HammingBytes(b1, b2)
+}
+
+func HammingBytes(b1 []byte, b2 []byte) int {
+	dist := 0
+	for i, b := range b1 {
+		c := b2[i]
+		for j := uint(0); j < 8; j++ {
+			if GetBit(b, j) != GetBit(c, j) {
+				dist++
+			}
+		}
+	}
+	return dist
+}
+
+func BreakRepeatingXOR(filename string) (key string, result string) {
+	dat, err := ioutil.ReadFile(filename)
+	check(err)
+	lines := strings.Split(string(dat), "\n")
+	full_text := strings.Join(lines, "")
+	barr, berr := base64.StdEncoding.DecodeString(full_text)
+	check(berr)
+	best_size := -1
+	best_dist := float32(1000)
+	for keysize := 2; keysize <= 40; keysize++ {
+		b1 := barr[:keysize]
+		b2 := barr[keysize : 2*keysize]
+		b3 := barr[2*keysize : 3*keysize]
+		b4 := barr[3*keysize : 4*keysize]
+		avg_dist := float32(HammingBytes(b1, b2) + HammingBytes(b2, b3) + HammingBytes(b3, b4) + HammingBytes(b1, b3) + HammingBytes(b1, b4) + HammingBytes(b2, b4))
+		avg_dist /= 6.0
+		avg_dist /= float32(keysize)
+		if avg_dist < best_dist {
+			best_dist = avg_dist
+			best_size = keysize
+		}
+	}
+	transposes := make([][]byte, best_size)
+	for i, b := range barr {
+		transposes[i%best_size] = append(transposes[i%best_size], b)
+	}
+	full_key := make([]uint8, 0)
+	for _, bs := range transposes {
+		key, _, _ := GuessKeyBytes(bs)
+		full_key = append(full_key, key)
+	}
+	key_str := string([]byte(full_key))
+	results_b, _ := hex.DecodeString(RepeatingKeyXOR(string(barr), key_str))
+	return key_str, string(results_b)
+}
+
+func main() {
+
+}
---
+++ crypto1_test.go
@@ -0,0 +1,87 @@
+// crypto1_test.go
+package main
+
+import "testing"
+
+func TestHexToBase64(t *testing.T) {
+	want := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	in := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	if HexToBase64(in) != want {
+		t.Errorf("HextToBase64() = %s, want %s", HexToBase64(in), want)
+	}
+}
+
+func TestFixedXOR(t *testing.T) {
+	want := "746865206b696420646f6e277420706c6179"
+	in1 := "1c0111001f010100061a024b53535009181c"
+	in2 := "686974207468652062756c6c277320657965"
+	result := FixedXOR(in1, in2)
+	if result != want {
+		t.Errorf("FixedXOR() = %s, want %s", result, want)
+	}
+}
+
+func TestGuessKey(t *testing.T) {
+	wanted_score := 19
+	wanted_key := uint8(88)
+	wanted_str := "Cooking MC's like a pound of bacon"
+	input := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	result_key, result_score, result_str := GuessKey(input)
+	if result_key != wanted_key {
+		t.Errorf("GuessKey() key = %d, want %d", result_key, wanted_key)
+	}
+	if result_score != wanted_score {
+		t.Errorf("GuessKey() score = %d, want %d", result_score, wanted_score)
+	}
+	if result_str != wanted_str {
+		t.Errorf("GuessKey() str = '%s', want '%s'", result_str, wanted_str)
+	}
+}
+
+func TestGuessWhich(t *testing.T) {
+	_, result := GuessWhich("/home/dan/Documents/Programming/Crypto/set1ch4.txt")
+	expected := "Now that the party is jumping\n"
+	if result != expected {
+		t.Errorf("GuessWhich() result = '%s', want '%s'", result, expected)
+	}
+}
+
+func TestRepeatingKeyXOR(t *testing.T) {
+	plain := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	key := "ICE"
+	encoded := RepeatingKeyXOR(plain, key)
+	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+	if encoded != expected {
+		t.Errorf("GuessWhich() result = '%s', want '%s'", encoded, expected)
+	}
+}
+
+func TestGetBit(t *testing.T) {
+	if !GetBit(byte(1), 0) {
+		t.Errorf("GetBit(1, 0) failed")
+	}
+	if GetBit(byte(1), 1) {
+		t.Errorf("GetBit(1, 1) failed")
+	}
+	if GetBit(byte(2), 0) {
+		t.Errorf("GetBit(2, 0) failed")
+	}
+	if !GetBit(byte(2), 1) {
+		t.Errorf("GetBit(2, 1) failed")
+	}
+}
+
+func TestHamming(t *testing.T) {
+	result := Hamming("this is a test", "wokka wokka!!!")
+	if result != 37 {
+		t.Errorf("Hamming() result = %d, want 37", result)
+	}
+}
+
+func TestBreakRepeatingXOR(t *testing.T) {
+	key, _ := BreakRepeatingXOR("/home/dan/Documents/Programming/Crypto/set1ch6.txt")
+	wanted := "Terminator X: Bring the noise"
+	if key != wanted {
+		t.Errorf("BreakRepeatingXOR() key was '%s', wanted '%s'", key, wanted)
+	}
+}
